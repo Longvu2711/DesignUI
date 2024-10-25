4cf09fe0fc166b2275e18531c505e2435613d8e5
Wanwamy <longvu1217@gmail.com>
2024-10-25 21:40:57 +0700

    Initial Commit

---
+++ DesignAndInterface/DesignAndInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignAndInterfaceApp.swift
+//  DesignAndInterface
+//
+//  Created by Wanmy on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignAndInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
